c90150c425c9a4d512def2fc46a1072bc1021ac2
IsakWinqvist <43164168+IsakWinqvist@users.noreply.github.com>
2018-11-05 14:08:52 +0200

    made many thinges)

--- index.php
+++ index.php
@@ -7,31 +7,142 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/css.css">
 
-    <title>Hello, world!</title>
+    <title>Fruit-Stop</title>
   </head>
   <body>
 	<div class="container">
 		<?php include 'header.php';?>
-		<h1>Hello, world!</h1>
-			<ul class="nav nav-tabs justify-content-center" role="tablist">
+    <header>
+      <nav class="navbar navbar-light bg-light">
+        <a style="font-size: 30px !important;" class="navbar-brand" href="#">
+          <img src="frootstob.png" width="50" height="45" class="d-inline-block align-top" alt="">
+        Fruit-Stop
+        </a>
+        <a class="btn btn-success" href="#" role="button">Login</a>
+      </nav>
+    </header>
+
+			<ul class="nav nav-pills nav-fill" role="tablist">
 			  <li class="nav-item">
-				<a class="nav-link active" data-toggle="tab" href="#frukt" role="tab">Frukt</a>
+				<a class="nav-link active btn-outline-secondary" data-toggle="tab" href="#frukt" role="tab">Frukt</a>
 			  </li>
 			  <li class="nav-item">
-				<a class="nav-link" data-toggle="tab" href="#grönsaker" role="tab">Grönsaker</a>
+				<a class="nav-link btn-outline-secondary" data-toggle="tab" href="#grönsaker" role="tab">Grönsaker</a>
 			  </li>
 			  <li class="nav-item">
-				<a class="nav-link" data-toggle="tab" href="#bär" role="tab">Bär</a>
+				<a class="nav-link btn-outline-secondary" data-toggle="tab" href="#bär" role="tab">Bär</a>
 			  </li>
 			</ul>
 
-			<!-- Tab panes -->
-			<div class="tab-content">
-			  <div class="tab-pane active" id="frukt" role="tabpanel">dsads</div>
-			  <div class="tab-pane" id="grönsaker" role="tabpanel">asdf</div>
-			  <div class="tab-pane" id="bär" role="tabpanel">qwerty</div>
+			<div class="b p-3 tab-content">
+			  <div class="tab-pane active" id="frukt" role="tabpanel">
+          <figure class="col-2 figure">
+            <img src="strawberry.png" width="100%" class="figure-img img-fluid rounded" alt="yes">
+            <figcaption class="figure-caption">Strawberries</figcaption>
+            <a class="btn btn-danger btn-sm">delete</a>
+            <a class="btn btn-success btn-sm">update</a>
+          </figure>
+
+          <figure class="col-2 figure">
+            <img src="avocado.png" width="100%" class="figure-img img-fluid rounded" alt="yes">
+            <figcaption class="figure-caption">Avogado))</figcaption>
+            <a class="btn btn-danger btn-sm">delete</a>
+            <a class="btn btn-success btn-sm">update</a>
+          </figure>
+
+          <figure class="col-2 figure">
+            <img src="grape.png" width="100%" class="figure-img img-fluid rounded" alt="yes">
+            <figcaption class="figure-caption">Grapes</figcaption>
+            <a class="btn btn-danger btn-sm">delete</a>
+            <a class="btn btn-success btn-sm">update</a>
+          </figure>
+
+          <figure class="col-2 figure">
+            <img src="watermelon.png" width="100%" class="figure-img img-fluid rounded" alt="yes">
+            <figcaption class="figure-caption">Watermelon</figcaption>
+            <a class="btn btn-danger btn-sm">delete</a>
+            <a class="btn btn-success btn-sm">update</a>
+          </figure>
+
+          <figure class="col-2 figure">
+            <img src="orange.png" width="100%" class="figure-img img-fluid rounded" alt="yes">
+            <figcaption class="figure-caption">Orange</figcaption>
+            <a class="btn btn-danger btn-sm">delete</a>
+            <a class="btn btn-success btn-sm">update</a>
+          </figure>
+
+          <figure class="col-2 figure">
+            <img src="banana.png" width="100%" class="figure-img img-fluid rounded" alt="yes">
+            <figcaption class="figure-caption">Banana</figcaption>
+            <a class="btn btn-danger btn-sm">delete</a>
+            <a class="btn btn-success btn-sm">update</a>
+          </figure>
+
+          <figure class="col-2 figure">
+            <img src="abble.png" width="100%" width="100%" class="figure-img img-fluid rounded" alt="yes">
+            <figcaption class="figure-caption">Apple</figcaption>
+            <a class="btn btn-danger btn-sm">delete</a>
+            <a class="btn btn-success btn-sm">update</a>
+          </figure>
+
+          <figure class="col-2 figure">
+            <img src="mango.png" width="100%" class="figure-img img-fluid rounded" alt="yes">
+            <figcaption class="figure-caption">Mango</figcaption>
+            <a class="btn btn-danger btn-sm">delete</a>
+            <a class="btn btn-success btn-sm">update</a>
+          </figure>
+
+          <figure class="col-2 figure">
+            <img src="pineapple.png" width="100%" class="figure-img img-fluid rounded" alt="yes">
+            <figcaption class="figure-caption">Pineapple</figcaption>
+            <a class="btn btn-danger btn-sm">delete</a>
+            <a class="btn btn-success btn-sm">update</a>
+          </figure>
+
+          <figure class="col-2 figure">
+            <img src="cherry.png" width="100%" class="figure-img img-fluid rounded" alt="yes">
+            <figcaption class="figure-caption">Cherries</figcaption>
+            <a class="btn btn-danger btn-sm">delete</a>
+            <a class="btn btn-success btn-sm">update</a>
+          </figure>
+
+        </div>
+
+        <div class="tab-pane" id="grönsaker" role="tabpanel">
+          <figure class="col-2 figure">
+            <img src="http://placehold.it/300" class="figure-img img-fluid rounded" alt="yes">
+            <figcaption class="figure-caption">Pineapple</figcaption>
+            <a class="btn btn-danger btn-sm">delete</a>
+            <a class="btn btn-success btn-sm">update</a>
+          </figure>
+
+          <figure class="col-2 figure">
+            <img src="http://placehold.it/300" class="figure-img img-fluid rounded" alt="yes">
+            <figcaption class="figure-caption">Pineapple</figcaption>
+            <a class="btn btn-danger btn-sm">delete</a>
+            <a class="btn btn-success btn-sm">update</a>
+          </figure>
+
 			</div>
+
+      <div class="tab-pane" id="bär" role="tabpanel">
+        <figure class="col-2 figure">
+          <img src="http://placehold.it/300" class="figure-img img-fluid rounded" alt="yes">
+          <figcaption class="figure-caption">Pineapple</figcaption>
+          <a class="btn btn-danger btn-sm">delete</a>
+          <a class="btn btn-success btn-sm">update</a>
+        </figure>
+
+        <figure class="col-2 figure">
+          <img src="http://placehold.it/300" class="figure-img img-fluid rounded" alt="yes">
+          <figcaption class="figure-caption">Pineapple</figcaption>
+          <a class="btn btn-danger btn-sm">delete</a>
+          <a class="btn btn-success btn-sm">update</a>
+        </figure>
+      </div>
+        use <kbd>ctrl+f</kbd> to find product
 	</div>
 
 
